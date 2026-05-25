3da388d3a2a4b6e61ea56e9512262313411160f7
Sark_oo7 <kosinick01@gmail.com>
2026-05-25 14:50:13 +0100

    chore: clean-up week 2 notebook

---
+++ fix-colab.py
@@ -0,0 +1,15 @@
+import nbformat
+
+notebook_path = "./notebooks/week2_data_prep.ipynb"
+
+with open(notebook_path, "r") as f:
+    nb = nbformat.read(f, as_version=4)
+
+# Remove the broken widget metadata
+if "widgets" in nb.metadata:
+    del nb.metadata["widgets"]
+
+with open(notebook_path, "w") as f:
+    nbformat.write(nb, f)
+
+print("Fixed.")
